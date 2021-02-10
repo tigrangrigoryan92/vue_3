99afe71bce0ffc1d1d283bb9cb5246dfa441ef41
TigranGrigoryan92 <vazgen.grigoryan@volo.global>
2021-02-10 15:54:15 +0400

    '04_vue_cli'

---
+++ 04_vue_cli/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hello Vue 3</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
